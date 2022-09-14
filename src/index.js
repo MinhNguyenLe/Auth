2ee29a573f22d9b56ea13debbec5aa5277260d95
MinhLee <minh.nguyenle1809@gmail.com>
2022-09-14 20:31:08 +0700

    init: Focus Auth page login register

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-// Internet Explorer 11 requires polyfills and partially supported by this project.
-// import 'react-app-polyfill/ie11';
-// import 'react-app-polyfill/stable';
-import ReactDOM from 'react-dom';
-import 'typeface-poppins';
-import './i18n';
-import './styles/app-base.css';
-import './styles/app-components.css';
-import './styles/app-utilities.css';
-import App from 'app/App';
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+//
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ----------------------------------------------------------------------
 
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+root.render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
+);
+
+// If you want to enable client cache, register instead.
 serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
